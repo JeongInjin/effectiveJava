b772aa817c6394f28f6e5cfd2406d2ab05202966
injinjeong <snowwwwww@naver.com>
2023-02-06 20:09:38 +0900

    4-22

---
+++ src/main/java/com/injin/me/effectivejava/chapter03/item22/constantutilityclass/Myclass.java
@@ -0,0 +1,11 @@
+package com.injin.me.effectivejava.chapter03.item22.constantutilityclass;
+
+import static com.injin.me.effectivejava.chapter03.item22.constantutilityclass.PhysicalConstants.*;
+
+public class Myclass {
+    public static void main(String[] args) {
+        double avogadrosNumber = PhysicalConstants.AVOGADROS_NUMBER;
+        double boltzmannConst = BOLTZMANN_CONST;
+        double electronMass = ELECTRON_MASS;
+    }
+}
