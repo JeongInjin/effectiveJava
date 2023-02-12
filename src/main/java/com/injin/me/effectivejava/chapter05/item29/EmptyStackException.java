1071077630a9b314cea9ade64c0d73812c6cbf7b
injinjeong <snowwwwww@naver.com>
2023-02-13 07:56:34 +0900

    5-29

---
+++ src/main/java/com/injin/me/effectivejava/chapter05/item29/EmptyStackException.java
@@ -0,0 +1,4 @@
+package com.injin.me.effectivejava.chapter05.item29;
+
+public class EmptyStackException extends RuntimeException {
+}
---
+++ src/main/java/com/injin/me/effectivejava/chapter05/item29/object/Stack.java
@@ -0,0 +1,4 @@
+package com.injin.me.effectivejava.chapter05.item29.object;
+
+public class Stack {
+}
