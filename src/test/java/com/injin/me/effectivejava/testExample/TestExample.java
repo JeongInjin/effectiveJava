39036c2e1f5fe4c9e935e41a30a67e5a642eecd6
injinjeong <snowwwwww@naver.com>
2023-02-15 08:00:58 +0900

    testExample

--- src/test/java/com/injin/me/effectivejava/testExample/TestExample.java
+++ src/test/java/com/injin/me/effectivejava/testExample/TestExample.java
@@ -2,10 +2,8 @@ package com.injin.me.effectivejava.testExample;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.lang.reflect.Field;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -58,10 +56,70 @@ public class TestExample {
         assertThat(numbers).isEqualTo(excepted);
     }
 
+    @Test
+    void finalReferenceChanges() {
+        final int i = 42;
+        //i = 43 error
+
+        final List<String> list = new ArrayList<>();
+        //list = new ArrayList<>(); error
+        assertThat(0).isEqualTo(list.size());
+
+        list.add("adding a new value into my list");
+        assertThat(1).isEqualTo(list.size());
+
+        list.clear();
+        assertThat(0).isEqualTo(list.size());
+    }
+
+    @Test
+    void staticVariableAccess() {
+        assertThat(7).isEqualTo(ExampleClass.EXAMPLE_VALUE);
+        ExampleClass c1 = new ExampleClass();
+        ExampleClass c2 = new ExampleClass();
+        c1.EXAMPLE_VALUE = 13; // 가능은 하지만 추천하지 않음.
+        assertThat(13).isEqualTo(ExampleClass.EXAMPLE_VALUE);
+        assertThat(c2.EXAMPLE_VALUE).isEqualTo(ExampleClass.EXAMPLE_VALUE);
+    }
+
+    @Test
+    void mutateBookRecordState() throws IllegalAccessException, NoSuchFieldException {
+        final BookRecord record = new BookRecord("jeong", "bestBook");
+        Field author = record.getClass().getDeclaredField("author");
+        author.setAccessible(true);
+        author.set(record, "injin");
+
+        assertThat("injin").isEqualTo(record.getAuthor());
+    }
+
+    public final class BookRecord {
+        private String author;
+        private String bookTitle;
+
+        public BookRecord(String author, String bookTitle) {
+            this.author = author;
+            this.bookTitle = bookTitle;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public String getBookTitle() {
+            return bookTitle;
+        }
+    }
+
+
+    public class ExampleClass {
+        public static int EXAMPLE_VALUE = 7;
+    }
+
     public class ReverseNumericalOrder implements Comparator<Integer> {
         @Override
         public int compare(Integer o1, Integer o2) {
             return o2 - o1;
         }
     }
+
 }
